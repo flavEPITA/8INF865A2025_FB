8f7c5e8f4c1c1114e169f1c960a991c3d8a78c83
Flavien B <flavien.baron@epita.fr>
2025-10-18 18:57:26 -0400

    Étape7

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -25,6 +25,7 @@ import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
 
 
 class MainActivity : ComponentActivity() {
@@ -37,7 +38,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingImage(message = "Happy Birthday Sam!", from = "From Emma", modifier = Modifier.padding(8.dp))
+                    GreetingImage(message = stringResource(R.string.happy_birthday_text), from = stringResource(
+                        R.string.signature_text), modifier = Modifier.padding(8.dp))
                 }
             }
         }
